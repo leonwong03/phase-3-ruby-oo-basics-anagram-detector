89a147b3068c8974351832b44812ae597fe10d6e
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-09-19 16:57:24 -0400

    commit tree 717426bc56f849f8aa78190bd9ad34b894823176

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end 
+    
+    attr_accessor :word
+    
+    def match(word_array)
+        word_array.select {|word| word.chars.sort == @word.chars.sort}
+    end
+end
